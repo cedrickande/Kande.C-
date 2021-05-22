31f96f297ed82a86e836eb14cb0af92356288562
arsene tadiumi <arsenetadiumi@gmail.com>
2021-05-22 15:08:14 +0200

    ma premiere modification dans ce systeme

---
+++ Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Calculator
+{
+	public double v = 0.0;
+  
+    public Calculator()
+	{
+	    
+	}
+
+	public double operation(int v1,int v2,char o)
+    {
+        switch (o)
+        {
+            case '+': return v1 + v2;
+                break;
+            case '/':
+                return v1 / v2;
+                break;
+            case '*':
+                return v1 * v2;
+                break;
+            case '-':
+                return v1 - v2;
+                break;
+        }
+    }
+}
